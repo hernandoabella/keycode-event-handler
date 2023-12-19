3fe7ed19c062727c46d1d3b37f861ceb2f9e95a4
Hernando Abella <hernandoabella@gmail.com>
2023-12-18 23:17:39 -0500

    Update script.js

--- script.js
+++ script.js
@@ -2,28 +2,28 @@ const insert = document.getElementById("insert");
 const number = document.getElementById("number");
 
 window.addEventListener("keydown", (e) => {
-  if (e.key === "Escape") {
-    number.style.display = "none";
-    return;
-  }
+	if (e.key === "Escape") {
+		number.style.display = "none";
+		return;
+	}
 
-  number.style.display = "block";
-  number.innerText = e.which;
+	number.style.display = "block";
+	number.innerText = e.which;
 
-  insert.innerHTML = `
-		  <div class="key__container">
-			  <div class="key">
-				  <div class="value">${e.key === " " ? "Space" : e.key}</div>
-				  <small>event.key</small>
-			  </div>
-			  <div class="key">
-				  <div class="value">${e.location}</div>
-				  <small>event.location</small>
-			  </div>
-			  <div class="key">
-				  <div class="value">${e.code}</div>
-				  <small>event.code</small>
-			  </div>
-		  </div>
-	  `;
+	insert.innerHTML = `
+	<div class="key__container">
+	<div class="key">
+		<div class="value">${e.key === " " ? "Space" : e.key}</div>
+		<small>event.key</small>
+	</div>
+	<div class="key">
+		<div class="value">${e.location}</div>
+		<small>event.location</small>
+	</div>
+	<div class="key">
+		<div class="value">${e.code}</div>
+		<small>event.code</small>
+	</div>
+	</div>	
+`;
 });
